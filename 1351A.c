3d41d20115a668518f18ed65b751199564df624f
xahid71 <jahid.info97@gmail.com>
2022-06-21 11:54:30 +0600

    1351A

---
+++ 1351A.c
@@ -0,0 +1,12 @@
+int main()
+{
+   int a = 0, b,c;
+   scanf("%d", &a);
+   for (int i = 1;  i<= a; i++) {
+       b = 0 ; c = 0;
+       scanf("%d", &b);
+       scanf("%d", &c);
+              
+        printf("%d\n", b+c );
+   }
+}
